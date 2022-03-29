787d2dfb4792162d0405a43dee37911c27ad9f56
osamaideegames <osamaideegames@gmail.com>
2022-03-29 12:41:13 +0500

    FB analytics

---
+++ Assets/_Project/Scripts/FacebookAnalytics.cs
@@ -0,0 +1,53 @@
+using Facebook.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacebookAnalytics : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Awake()
+    {
+        if (FB.IsInitialized)
+        {
+            FB.ActivateApp();
+        }
+        else
+        {
+            //Handle FB.Init
+            FB.Init(() => {
+                FB.ActivateApp();
+            });
+        }
+    }
+    void OnApplicationPause(bool pauseStatus)
+    {
+        // Check the pauseStatus to see if we are in the foreground
+        // or background
+        if (!pauseStatus)
+        {
+            //app resume
+            if (FB.IsInitialized)
+            {
+                FB.ActivateApp();
+            }
+            else
+            {
+                //Handle FB.Init
+                FB.Init(() => {
+                    FB.ActivateApp();
+                });
+            }
+        }
+    }
+}
