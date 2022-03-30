2712139d6f354e9ab7544478ea202315991e107c
osamaideegames <osamaideegames@gmail.com>
2022-03-30 16:34:47 +0500

    Update DisasterHandler.cs

--- Assets/_Project/Scripts/Game Specific/DisasterHandler.cs
+++ Assets/_Project/Scripts/Game Specific/DisasterHandler.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class DisasterHandler : MonoBehaviour
 {
-    public LevelsManager levelsManager;
+    
     public GameObject stormObj;
     public GameObject volcanoObj;
     public GameObject lightningOBJ;
@@ -14,7 +14,7 @@ public class DisasterHandler : MonoBehaviour
         Toolbox.GameplayScript.FinalDecisionHandling(5);
         Toolbox.GameplayScript.camShake.ShakeCamera(5, 4);
         Toolbox.GameplayScript.StartBuildingDistruction();
-        levelsManager.CurLevelHandler.tutorials.SetActive(false);
+        Toolbox.GameplayScript.levelsManager.CurLevelHandler.tutorials.SetActive(false);
     }
 
     public void Tsunami()
@@ -23,13 +23,13 @@ public class DisasterHandler : MonoBehaviour
         //Instantiate(tsunamiObj, transform);
         Toolbox.GameplayScript.FinalDecisionHandling(14);
         tsunamiObj.SetActive(true);
-        levelsManager.CurLevelHandler.tutorials.SetActive(false);
+        Toolbox.GameplayScript.levelsManager.CurLevelHandler.tutorials.SetActive(false);
     }
     public void Volcano()
     {
         //Toolbox.GameplayScript.camShake.ShakeCamera(2, 4);
         //Instantiate(volcanoObj, transform);
-        levelsManager.CurLevelHandler.tutorials.SetActive(false);
+        Toolbox.GameplayScript.levelsManager.CurLevelHandler.tutorials.SetActive(false);
         Toolbox.GameplayScript.FinalDecisionHandling(13);
         volcanoObj.SetActive(true);
     }
@@ -37,13 +37,13 @@ public class DisasterHandler : MonoBehaviour
     {
         Toolbox.GameplayScript.FinalDecisionHandling(15);
         tornadoObj.SetActive(true);
-        levelsManager.CurLevelHandler.tutorials.SetActive(false);
+        Toolbox.GameplayScript.levelsManager.CurLevelHandler.tutorials.SetActive(false);
         Toolbox.GameplayScript.camBrain.gameObject.SetActive(false);
 
     }
     public void Storm()
     {
-        levelsManager.CurLevelHandler.tutorials.SetActive(false);
+        Toolbox.GameplayScript.levelsManager.CurLevelHandler.tutorials.SetActive(false);
         Toolbox.GameplayScript.camShake.ShakeCamera(1, 4);
         Instantiate(stormObj, transform);
     }
